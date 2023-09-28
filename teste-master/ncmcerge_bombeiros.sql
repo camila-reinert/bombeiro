64dd95ca60cd0cb99f1db37cb90b05a461b6402f
camilinha <124690175+camila-reinert@users.noreply.github.com>
2023-09-28 08:15:17 -0300

    Add files via upload

---
+++ teste-master/ncmcerge_bombeiros.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18/09/2023 às 22:36
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ncmcerge_bombeiros`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `anamnese`
+--
+
+CREATE TABLE `anamnese` (
+  `id_anamnese` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `anamnese_gestacional`
+--
+
+CREATE TABLE `anamnese_gestacional` (
+  `id_anamnese_gestacional` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `avaliacao_paciente_maior`
+--
+
+CREATE TABLE `avaliacao_paciente_maior` (
+  `id_avaliacao_paciente_maior` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `avaliacao_paciente_menor`
+--
+
+CREATE TABLE `avaliacao_paciente_menor` (
+  `id_avaliacao_paciente_menor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastro`
+--
+
+CREATE TABLE `cadastro` (
+  `cod_cadastro` int(11) NOT NULL,
+  `senha_cadastro` varchar(45) NOT NULL,
+  `nome_cadastro` varchar(45) NOT NULL,
+  `Acesso_cadastro` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cadastro`
+--
+
+INSERT INTO `cadastro` (`cod_cadastro`, `senha_cadastro`, `nome_cadastro`, `Acesso_cadastro`) VALUES
+(123456, 'batata', 'Nylton', 's');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `decisao_transporte`
+--
+
+CREATE TABLE `decisao_transporte` (
+  `id_decisao_transporte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `emergencias`
+--
+
+CREATE TABLE `emergencias` (
+  `id_emergencias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `forma_conducao`
+--
+
+CREATE TABLE `forma_conducao` (
+  `id_forma_conducao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `localizacao_traumas_maior`
+--
+
+CREATE TABLE `localizacao_traumas_maior` (
+  `id_localizacao_traumas_maior` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `localizacao_traumas_menor`
+--
+
+CREATE TABLE `localizacao_traumas_menor` (
+  `id_localizacao_traumas_menor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `materiais_utilizados`
+--
+
+CREATE TABLE `materiais_utilizados` (
+  `id_materiais_utilizados` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `objetos_recolhidos`
+--
+
+CREATE TABLE `objetos_recolhidos` (
+  `id_objetos_recolhidos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id_paciente` int(11) NOT NULL,
+  `nome_hospital_paciente` varchar(45) NOT NULL,
+  `data_paciente` date NOT NULL,
+  `sexo_paciente` varchar(1) NOT NULL,
+  `nome_paciente` varchar(45) NOT NULL,
+  `idade_paciente` int(11) NOT NULL,
+  `rg_cpf_paciente` varchar(45) NOT NULL,
+  `fone_paciente` varchar(45) NOT NULL,
+  `acompanhante_paciente` varchar(45) NOT NULL,
+  `idade_acompanhante_paciente` int(11) NOT NULL,
+  `local_ocorrencia_paciente` varchar(45) NOT NULL,
+  `n_usb_paciente` varchar(45) NOT NULL,
+  `cod_ur_paciente` varchar(45) NOT NULL,
+  `n_ocorr_paciente` varchar(45) NOT NULL,
+  `cod_ps_paciente` varchar(1) NOT NULL,
+  `desp_paciente` varchar(45) NOT NULL,
+  `h_ch_paciente` varchar(45) NOT NULL,
+  `km_final_paciente` varchar(45) NOT NULL,
+  `cod_sia_sus_paciente` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `paciente`
+--
+
+INSERT INTO `paciente` (`id_paciente`, `nome_hospital_paciente`, `data_paciente`, `sexo_paciente`, `nome_paciente`, `idade_paciente`, `rg_cpf_paciente`, `fone_paciente`, `acompanhante_paciente`, `idade_acompanhante_paciente`, `local_ocorrencia_paciente`, `n_usb_paciente`, `cod_ur_paciente`, `n_ocorr_paciente`, `cod_ps_paciente`, `desp_paciente`, `h_ch_paciente`, `km_final_paciente`, `cod_sia_sus_paciente`) VALUES
+(24, 'São José', '2023-09-20', 'm', 'Nylton Eduardo Constâncio', 18, '14833715902', '989130277', 'Gabriel Elesbao', 18, 'Senai Sul joinville', '21856', 's', '4891231', 'n', 'Sim', 'aasda', 'asdwasd', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `problemas_suspeitos`
+--
+
+CREATE TABLE `problemas_suspeitos` (
+  `id_problemas_suspeitos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `procedimentos_efetuados`
+--
+
+CREATE TABLE `procedimentos_efetuados` (
+  `id_procedimentos_efetuados` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sinais_e_sintomas`
+--
+
+CREATE TABLE `sinais_e_sintomas` (
+  `id_sinais_e_sintomas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sinais_vitais`
+--
+
+CREATE TABLE `sinais_vitais` (
+  `id_sinais_vitais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cadastro`
+--
+ALTER TABLE `cadastro`
+  ADD PRIMARY KEY (`cod_cadastro`);
+
+--
+-- Índices de tabela `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id_paciente`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
